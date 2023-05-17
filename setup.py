b0deb149f5f5c7d1371ddc994c39b249e66a850d
Levi Rogalla <levi.rogalla@gmail.com>
2023-05-17 13:19:02 -0400

    Addad relu and sigmoid funtions

--- setup.py
+++ setup.py
@@ -1,12 +1,37 @@
-from Python_Linear_Algebra.main import Matrix
+from Python_Linear_Algebra.main import Matrix, Vector
+import random
+
+
+ITERATIONS = 100
+MAXAGRESSION = 1
+INITWEIGHT = 1
+
+def getRandom(): 
+    randomNum = random.randint(-10,10) 
+    if randomNum:
+        return randomNum
+    else:
+        return getRandom()
+    
+
+# one node
+layer1 = Vector()
+weights12 = Matrix([getRandom()], [getRandom()])
+# two nodes
+layer2 = Vector()
+weights23 = Matrix([getRandom(), getRandom()])
+# one node
+layer3 = Vector()
+
 
 def f(x): 
     return ((3*(x**2) - 15) + 2**x)
 def g(x):
     return 2*x
+def n(x):
+    return ((x**2) + 5)
 def h(x):
     return [1,2]
-ITERATIONS = 9999
 
 print("Creating training data")
 
@@ -14,7 +39,7 @@ def createData(iterations):
     data = []
     for x in range(iterations):
         if x:
-            data.append(h(x))
+            data.append([x, n(x)])
     return Matrix(*data)
 
 
--- threelayer.py
+++ threelayer.py
@@ -1,31 +1,38 @@
 from Python_Linear_Algebra.main import Matrix, Vector
-from setup import TRAIN
-import random
+from setup import TRAIN, MAXAGRESSION 
+from setup import layer1, layer2, layer3, weights12, weights23
+import math
 
 print("Training network")
 
-AGRESSION = 0.1
+agression = 0.1
 
-# one node
-layer1 = Vector()
-weights12 = Matrix([2], [2])
-# two nodes
-layer2 = Vector()
-weights23 = Matrix([2, 2])
-# one node
-layer3 = Vector()
-
-
+def relu(activation: Vector) -> Vector:
+    newActivation = []
+    for val in activation:  
+        if val < 0:
+            newActivation.append(0)
+        else:
+            newActivation.append(val)
+    return Vector(*newActivation)
+    
+def sigmoid(activation: Vector) -> Vector:
+    newActivation = []
+    for val in activation:
+        newVal = 1/(1+math.exp(-1*val))
+        newActivation.append(newVal)
+    return Vector(*newActivation)
 
 def calcLayer(layer: Vector, weights: Matrix) -> Vector:
     return  weights * layer
+
 
 def calcCost(activation: Vector, expected: Vector) -> dict[Vector, Vector]:
     costVal = []
     costDer = []
 
     for i, val in enumerate(activation):
-        costVal.append(((val-expected[i])**2)/expected[i])
+        costVal.append(((val-expected[i])**2)/expected[i] if expected[i] else ((val-expected[i])**2))
         costDer.append(((val-expected[i])*2))
 
     return {
@@ -40,7 +47,7 @@ def calcNewWeight(cost: Vector, weights: Matrix) -> Matrix:
         newRow = []
         if cost['derivative'][r] == 0:
             return Matrix(*weights)
-        change = (cost['value'][r])/(cost['derivative'][r])*AGRESSION
+        change = (cost['value'][r])/(cost['derivative'][r])*MAXAGRESSION
         for val in row:
             newRow.append(
                 (val-change)
@@ -49,43 +56,48 @@ def calcNewWeight(cost: Vector, weights: Matrix) -> Matrix:
 
     return Matrix(*newWeights)
 
+def adjustAgression(output, expected):
+    totalCost = (expected - output)**2
+    costDer = (expected - output) * 2
+    agression = totalCost/costDer if costDer else 0.00001
+    return (agression**2 if abs(agression) < MAXAGRESSION else MAXAGRESSION)
+
 file = open("data.txt", "w")
 for i, data in enumerate(TRAIN):
-
     # normalizing dataset
     val1 = data[0] 
     val2 = data[1]
 
-    print("\nCalculating layers")
     layer1 = Vector(val1)
-    file.write(f"{layer1}\n")
-    print(f"layer1: {layer1}")
-    print(f"weights from 1 to 2: {weights12}")
-    layer2 = calcLayer(layer1, weights12)
-    file.write(f"{layer2}\n")
-    print(f"layer2: {layer2}")
-    print(f"weights from 2 to 3: {weights23}")
+    file.write(f"{layer1}")
+    file.write(f"{weights12}")
+
+    layer2 = relu(calcLayer(layer1, weights12))
+    file.write(f"{layer2}")
+    file.write(f"{weights23}")
+
     layer3 = calcLayer(layer2, weights23)
     file.write(f"{layer3}\n\n")
-    print(f"layer3: {layer3}")
  
-
-    print("\nCalculating weights and cost")
     cost32 = calcCost(layer3, Vector(val2))
-    print(f"the cost from 3 to 2: {cost32}")
 
     weights23 = calcNewWeight(cost32, weights23)
-    print(f"new weights from 2 to 3: {weights23}")
-    betterLayer2 = (weights23.T())*(layer3)
+
+    # pass val2 or layer 3 into this?
+    betterLayer2 = relu((weights23.T())*(val2))
     cost21 = calcCost(layer2, betterLayer2)
-    print(f"the cost from 2 to 1: {cost21}")
+
     weights12 = calcNewWeight(cost21, weights12)
-    print(f"new weights from 1 to 2: {weights12}\n\n")
+ 
 
 
 print(f"Final test: {layer1}")
-print(weights12)
-print(layer2)
-print(weights23)
+print(f"Weights from 1 to 2: {weights12}")
+print(f"Layer 2: {layer2}")
+print(f"Weights from 2 to 3: {weights23}")
 print(f"Final output: {layer3}")
+
+print(f"Finished with accuracy of: {layer3[0]/TRAIN[-1][1]}")
+
 file.close()
+
